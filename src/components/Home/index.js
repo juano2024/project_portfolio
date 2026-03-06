95ebdd2052927191bc311fd6571412c9f1c7d5fe
juano2024 <juan_manuel.ospina@uao.edu.co>
2026-03-05 20:06:33 -0500

    Implementación de estilos en la parte de home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,19 @@
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
+import { Link } from 'react-router-dom';
 const Home = () => {
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
+                <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer" />
+                lobodan
+                <br />
+                Frontend Developer / JavaScript Expert / React Enthusiast
+                </h1>
+                <h2>Web developer</h2>
+                <Link to= "/contact" className='flat-button'>Contact me</Link>
             </div>
 
         </div>
