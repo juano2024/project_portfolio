b7288e63cb351df6f39baf0d5fa4034f3f5b3c7f
juano2024 <juan_manuel.ospina@uao.edu.co>
2026-03-07 16:31:11 -0500

    Animaciones cheveres

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,42 @@
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
-    const [letterClass] = useState('text-animate');
+    const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
-    const jobArray = ['F', 'r', 'o', 'n', 't', 'e', 'n', 'd', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+    const jobArray = [
+        'F', 
+        'r', 
+        'o', 
+        'n', 
+        't', 
+        'e', 
+        'n', 
+        'd', 
+        ' ', 
+        'D', 
+        'e', 
+        'v', 
+        'e', 
+        'l', 
+        'o', 
+        'p', 
+        'e', 
+        'r'];
+
+        useEffect(() => {
+        
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+        
+        return () => {
+                    clearTimeout(timeoutId)
+                }
+    }, [])
 
     return (
         <div className="container home-page">
@@ -21,7 +50,7 @@ const Home = () => {
                 <img src={LogoTitle} alt="developer" />
                 <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
                 <br />
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} />
                 </h1>
                 <h2>Web Developer / JavaScript Expert / React Enthusiast</h2>
                 <Link to= "/contact" className='flat-button'>Contact me</Link>
