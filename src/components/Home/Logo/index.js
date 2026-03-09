d8626a1a237e836aa076f26c6e4fea533f64f303
juano2024 <juan_manuel.ospina@uao.edu.co>
2026-03-08 21:55:56 -0500

    eliminacion de gsap-trail

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -16,6 +16,7 @@ const Logo = () => {
     .from(outlineLogoRef.current, {drawSVG: 0, duration: 20,})
     gsap.fromTo(solidLogoRef.current, {opacity: 0}, {opacity: 1, duration: 4, delay: 4})
   },[])
+  
   return (
     <div className="logo-container" ref={bgRef}>
       <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt="S" />
